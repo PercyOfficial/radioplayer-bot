d618565f0b2c4935dfd4249e69eefb7e92d8a6e5
slgeekshow <86388269+youtubeslgeekshow@users.noreply.github.com>
2021-07-15 23:12:54 -0700

    Update radio.py

--- plugins/radio.py
+++ plugins/radio.py
@@ -1,6 +1,6 @@
 """
-VC Music Player, Telegram Voice Chat Userbot
-Copyright (C) 2021  Zaute Km | TGVCSETS
+radio player, Telegram Voice Chat Userbot
+Copyright (C) 2021  youtubeslgeekshow
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as published by
@@ -26,23 +26,27 @@ ADMINS=Config.ADMINS
 @Client.on_message(filters.command(["radio", f"radio@{USERNAME}"]) & filters.user(ADMINS) & (filters.chat(CHAT) | filters.private))
 async def radio(client, message: Message):
     if 1 in RADIO:
-        k=await message.reply_text("Kindly stop existing Radio Stream /stopradio")
+        k=await message.reply_sticker("CAACAgIAAxkBAAELwrJg8SMFwQJtdD0lCZ1T03ugKWjeuQACfQMAAkcVaAmGhFvuAnOhoR4E")
+        await message.reply_text("⭕️ Kindly stop existing Radio Stream /stopradio ⭕️ ")
         await mp.delete(k)
         await message.delete()
         return
     await mp.start_radio()
-    k=await message.reply_text(f"Started Radio: <code>{STREAM}</code>")
+    k=await message.reply_sticker("CAACAgIAAxkBAAELwrlg8SNArwlX-LzIAcptlqsyBm5hJgACHAADlp-MDpnUab5i8nnlHgQ")
+    await message.reply_text(f"✅Started Radio: <code>{STREAM}</code> ✅")
     await mp.delete(k)
     await message.delete()
 
 @Client.on_message(filters.command(['stopradio', f"stopradio@{USERNAME}"]) & filters.user(ADMINS) & (filters.chat(CHAT) | filters.private))
 async def stop(_, message: Message):
     if 0 in RADIO:
-        k=await message.reply_text("Kindly start Radio First /radio")
+        k=await message.reply_sticker("CAACAgIAAxkBAAELwrlg8SNArwlX-LzIAcptlqsyBm5hJgACHAADlp-MDpnUab5i8nnlHgQ")
+        await message.reply_text("🔊 Kindly start Radio First /radio 🔊 ")
         await mp.delete(k)
         await message.delete()
         return
     await mp.stop_radio()
-    k=await message.reply_text("Radio stream ended.")
+    k=await message.reply_sticker("CAACAgIAAxkBAAELwrJg8SMFwQJtdD0lCZ1T03ugKWjeuQACfQMAAkcVaAmGhFvuAnOhoR4E")
+    await message.reply_text("Radio stream ended 🔕.")
     await mp.delete(k)
     await message.delete()
